2a4364624a5adfbd8f7d53fcd5311abba62b1ed9
Shunya <83391015+PythonUser-Shunya@users.noreply.github.com>
2021-11-22 17:42:20 +0900

    Update function_QR_Barcode.py

--- function_QR_Barcode.py
+++ function_QR_Barcode.py
@@ -1,4 +1,5 @@
 # 実行は別ファイルから
+# 以下exe化のコマンド
 # pyinstaller QR_Barcode.py --onefile --noconsole --add-binary "C:\Users\shuny\anaconda3\envs\barcode\Lib\site-packages\pyzbar\*.dll;pyzbar"
 from pyzbar.pyzbar import decode
 import cv2
@@ -7,7 +8,8 @@ from collections import Counter
 import numpy as np
 import warnings
 import openpyxl
-import os
+from copy import copy
+# import os
 
 # 警告を非表示にする
 warnings.simplefilter("ignore")
@@ -17,8 +19,10 @@ number_list = []
 def save_battery(read_list):
     np.savetxt("battery_list.csv", read_list, delimiter=",", fmt="%s")
 
+
 def save_SD(read_list):
     np.savetxt("SD_list.csv", read_list, delimiter=",", fmt="%s")
+
 
 def save_camera(read_list):
     np.savetxt("camera_list.csv", read_list, delimiter=",", fmt="%s")
@@ -32,12 +36,14 @@ def load_battery_list():
     load_list = list(set(load_list))
     return load_list
 
+
 def load_SD_list():
     file = "SD_list.csv"
     load_list = list(np.loadtxt(
         file, delimiter=",", dtype="unicode", ndmin=1))
     load_list = list(set(load_list))
     return load_list
+
 
 def load_camera_list():
     file = "camera_list.csv"
@@ -55,12 +61,14 @@ def make_battery_list():
         compiled_list = []
     return compiled_list
 
+
 def make_SD_list():
     try:
         compiled_list = load_SD_list()
     except OSError:
         compiled_list = []
     return compiled_list
+
 
 def make_camera_list():
     try:
@@ -78,9 +86,9 @@ def make_new_excel_battery(xlsx_file_path, place, read_list):
         if str(cell_a.value) in read_list:
             cell_a_row = cell_a.row
             ws[f"C{cell_a_row}"].value = place
-    change_xlsx_file_path = os.path.basename(xlsx_file_path)
-    wb.save(change_xlsx_file_path)
+    wb.save(xlsx_file_path)
     return True
+
 
 def make_new_excel_SD(xlsx_file_path, place, read_list, user):
     wb = openpyxl.load_workbook(xlsx_file_path)
@@ -90,20 +98,23 @@ def make_new_excel_SD(xlsx_file_path, place, read_list, user):
             cell_a_row = cell_a.row
             ws[f"E{cell_a_row}"].value = place
             ws[f"F{cell_a_row}"].value = user
-    change_xlsx_file_path = os.path.basename(xlsx_file_path)
-    wb.save(change_xlsx_file_path)
+    wb.save(xlsx_file_path)
     return True
+
 
 def make_new_excel_camera(xlsx_file_path, place, read_list, user):
     wb = openpyxl.load_workbook(xlsx_file_path)
     ws = wb.worksheets[0]
+    border = copy(ws['A1'].border)
     for cell_a in ws["A"]:
         if str(cell_a.value) in read_list:
             cell_a_row = cell_a.row
             ws[f"B{cell_a_row}"].value = place
             ws[f"C{cell_a_row}"].value = user
-    change_xlsx_file_path = os.path.basename(xlsx_file_path)
-    wb.save(change_xlsx_file_path)
+            # 罫線
+            ws[f"B{cell_a_row}"].border = border
+            ws[f"C{cell_a_row}"].border = border
+    wb.save(xlsx_file_path)
     return True
 
 
@@ -142,37 +153,50 @@ def over30_and_under5_list(number_list):
     ]
     return number_list_counter_30
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+# 読み取った総数を表示する関数
+def print_total_number(frame, compiled_list):
+    cv2.putText(frame, f'COUNT: {len(compiled_list)}', (0, 35),
+                font, 1, (0, 0, 0), 3, cv2.LINE_AA)
+    cv2.putText(frame, f'COUNT: {len(compiled_list)}', (0, 35),
+                font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    cv2.imshow('frame', frame)
+
+# バーコードに四角を描画、読み込んだ情報を表示する関数
+def drw_rectangle(frame, barcodeData, x, y, w, h):
+    cv2.rectangle(frame, (x, y), (x+w, y+h),(0, 0, 255), 2)
+    cv2.rectangle(frame, (x, y-25), (x+60, y), (0, 0, 255), -1)
+    cv2.putText(frame, barcodeData, (x, y-10), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
 
 # 電池のバーコードを読み取る
 def read_battery(camera_number, compiled_list):
+    # 数字のみ読み取れるようにする
     re_compile = re.compile('^[0-9]+$')
     cap = cv2.VideoCapture(int(camera_number))
-    font = cv2.FONT_HERSHEY_SIMPLEX
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
             d = decode(frame)
             if d:
                 for barcode in d:
+                    # code128のみ許す
                     if barcode.type == 'CODE128':
                         x, y, w, h = barcode.rect
-                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-                        cv2.rectangle(frame, (x, y-25), (x+60, y), (0, 0, 255), -1)
                         barcodeData = barcode.data.decode('utf-8')
-                        frame = cv2.putText(
-                            frame, barcodeData, (x, y-10), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
+                        # 四角を描画
+                        drw_rectangle(frame, barcodeData, x, y, w, h)
+                        # 読み取った情報を追加する
                         number_list.append(barcodeData)
+                        # 読み取った情報にフィルターをかける
                         number_list_counter_30 = over30_and_under4(
                             number_list, barcode)
+                        # さらに数字のみしか許さない
                         for c in filter(re_compile.match, number_list_counter_30):
                             compiled_list.append(c)
+                            # 重複を許さない
                             compiled_list = list(set(compiled_list))
-            # 背景になる
-            cv2.putText(frame, f'COUNT: {len(compiled_list)}', (0, 35),
-                        font, 1, (0, 0, 0), 3, cv2.LINE_AA)
-            cv2.putText(frame, f'COUNT: {len(compiled_list)}', (0, 35),
-                        font, 1, (255, 255, 255), 1, cv2.LINE_AA)
-            cv2.imshow('frame', frame)
+            # 読み取った総数を表示する
+            print_total_number(frame, compiled_list)
         key = cv2.waitKey(10)
         if key == 27:  # ESCkey
             break
@@ -186,31 +210,30 @@ def read_battery(camera_number, compiled_list):
 def read_camera(camera_number, compiled_list):
     re_compile = re.compile('^[0-9]+$')
     cap = cv2.VideoCapture(int(camera_number))
-    font = cv2.FONT_HERSHEY_SIMPLEX
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
             d = decode(frame)
             if d:
                 for barcode in d:
+                    # QRのみ許す
                     if barcode.type == 'QRCODE':
                         x, y, w, h = barcode.rect
-                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-                        cv2.rectangle(frame, (x, y-25), (x+60, y), (0, 0, 255), -1)
                         barcodeData = barcode.data.decode('utf-8')
-                        frame = cv2.putText(
-                            frame, barcodeData, (x, y-10), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
+                        # 四角を描画
+                        drw_rectangle(frame, barcodeData, x, y, w, h)
+                        # 読み取った情報を追加する
                         number_list.append(barcodeData)
+                        # 読み取った情報にフィルターをかける
                         number_list_counter_30 = over30_and_under4(
                             number_list, barcode)
+                        # さらに数字のみしか許さない
                         for c in filter(re_compile.match, number_list_counter_30):
                             compiled_list.append(c)
+                            # 重複を許さない
                             compiled_list = list(set(compiled_list))
-            cv2.putText(frame, f'COUNT: {len(compiled_list)}', (0, 35),
-                        font, 1, (0, 0, 0), 3, cv2.LINE_AA)
-            cv2.putText(frame, f'COUNT: {len(compiled_list)}', (0, 35),
-                        font, 1, (255, 255, 255), 1, cv2.LINE_AA)
-            cv2.imshow('frame', frame)
+            # 読み取った総数を表示する
+            print_total_number(frame, compiled_list)
         key = cv2.waitKey(10)
         if key == 27:  # ESCkey
             break
@@ -223,30 +246,29 @@ def read_camera(camera_number, compiled_list):
 # SD用
 def read_SD(camera_number, compiled_list):
     cap = cv2.VideoCapture(int(camera_number))
-    font = cv2.FONT_HERSHEY_SIMPLEX
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
             d = decode(frame)
             if d:
                 for barcode in d:
+                    # QRのみ許す
                     if barcode.type == 'QRCODE':
                         x, y, w, h = barcode.rect
-                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-                        cv2.rectangle(frame, (x, y-25), (x+60, y), (0, 0, 255), -1)
                         barcodeData = barcode.data.decode('utf-8')
-                        frame = cv2.putText(
-                            frame, barcodeData, (x, y-10), font, .5, (255, 255, 255), 1, cv2.LINE_AA)
+                        # 四角を描画
+                        drw_rectangle(frame, barcodeData, x, y, w, h)
+                        # 読み取った情報を追加する
                         number_list.append(barcodeData)
+                        # 読み取った情報にフィルターをかける
                         number_list_counter_30 = over30_and_under5_list(
                             number_list)
+                        # フィルターを通過した数字のみリストに追加
                         compiled_list += (number_list_counter_30)
+                        # 重複を許さない
                         compiled_list = list(set(compiled_list))
-            cv2.putText(frame, f'COUNT: {len(compiled_list)}', (0, 35),
-                        font, 1, (0, 0, 0), 3, cv2.LINE_AA)
-            cv2.putText(frame, f'COUNT: {len(compiled_list)}', (0, 35),
-                        font, 1, (255, 255, 255), 1, cv2.LINE_AA)
-            cv2.imshow('frame', frame)
+            # 読み取った総数を表示する
+            print_total_number(frame, compiled_list)
         key = cv2.waitKey(10)
         if key == 27:  # ESCkey
             break
